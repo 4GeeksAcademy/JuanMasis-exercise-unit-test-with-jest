ae658d35ce9cc56abf232bce0bb9f3fde149674f
juanMasis777 <juanmasisg@gmail.com>
2026-03-12 22:21:12 +0000

    Project done

---
+++ .learn/exercises/04-our-first-function/app.js
@@ -0,0 +1,10 @@
+// This is my function that sums two numbers
+const sum = (a,b) => {
+  return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files
+module.exports = { sum };
